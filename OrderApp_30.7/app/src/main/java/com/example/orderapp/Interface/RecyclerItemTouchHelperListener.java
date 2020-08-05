2aa76f39d27379525a740a971c8611305502b446
thanhbkcse <67247762+thanhbkcse@users.noreply.github.com>
2020-08-05 11:05:33 +0700

    Final

--- OrderApp_30.7/app/src/main/java/com/example/orderapp/Interface/RecyclerItemTouchHelperListener.java
+++
@@ -1,8 +0,0 @@
-package com.example.orderapp.Interface;
-
-import androidx.recyclerview.widget.RecyclerView;
-
-public interface RecyclerItemTouchHelperListener {
-
-    void onSwiped(RecyclerView.ViewHolder viewHolder, int direction, int position);
-}
